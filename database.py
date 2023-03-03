eec578baf9df87809793cd8f4b554977bb590840
kiyakeynia8 <118113533+kiyakeynia8@users.noreply.github.com>
2023-03-03 11:28:01 +0330

    Add files via upload

--- database.py
+++ database.py
@@ -7,14 +7,14 @@ class Database():
         self.tasks = []
 
     def get_tasks(self):
-        query = "SELECT * FROM tasks"
-        result = self.cursor.execute("SELECT * FROM tasks")
+        query = "SELECT * FROM tasks ORDER BY is_done"
+        result = self.cursor.execute(query)
         self.tasks = result.fetchall()
         return self.tasks
 
-    def add_new_task(self,new_title,new_description):
+    def add_new_task(self,new_title,new_description,new_date,new_time,i_o_n):
         try:
-            query = f"INSERT INTO tasks(title, description)VALUES('{new_title}', '{new_description}')"
+            query = f"INSERT INTO tasks(title, description, date, time, i_o_n)VALUES('{new_title}', '{new_description}', '{new_date}', '{new_time}', '{i_o_n}')"
             self.cursor.execute(query)
             self.con.commit()
             return True
@@ -23,14 +23,25 @@ class Database():
             return False
 
     def remove_task(self,id):
-        self.cursor.execute(f"DELETE FROM tasks WHERE id = '{id}'")
-        self.con.commit()
+        try:
+            self.cursor.execute(f"DELETE FROM tasks WHERE id = '{id}'")
+            self.con.commit()
+            return True
+
+        except:
+            return False
     
     def task_done(self,id):
-        self.cursor.execute(f'SELECT * FROM tasks WHERE id = "{id}"')
-        task = self.cursor.fetchall()
-        if task != None:
-            new_id = len(self.tasks) + 1
-            self.cursor.execute(f'UPDATE tasks SET is_done = 1 WHERE id = {id}')
-            self.cursor.execute(f'UPDATE tasks SET id = {new_id} WHERE is_done = 1')
-            self.con.commit()
+        try:
+            self.cursor.execute(f'SELECT * FROM tasks WHERE id = "{id}"')
+            task = self.cursor.fetchall()
+            if task != None:
+                new_id = len(self.tasks) + id
+                self.cursor.execute(f'UPDATE tasks SET is_done = 1 WHERE id = {id}')
+                self.con.commit()
+                self.cursor.execute(f'UPDATE tasks SET id = {new_id} WHERE is_done = 1')
+                self.con.commit()
+                return True
+        
+        except:
+            return False
--- main.py
+++ main.py
@@ -19,12 +19,40 @@ class MainWindow(QMainWindow):
     def new_tasks(self):
         new_title = self.ui.tb_ne_task.text()
         new_description = self.ui.tb_n_t_d.toPlainText()
-        feedback = self.db.add_new_task(new_title, new_description)
+        new_date = self.ui.date.text()
+        new_time = self.ui.time.text()
+        i_o_n = self.ui.i_o_n.text()
+        feedback = self.db.add_new_task(new_title, new_description, new_date, new_time, i_o_n) 
 
         if feedback == True:
             self.read_from_database()
             self.ui.tb_ne_task.setText("")
             self.ui.tb_n_t_d.setText("")
+            self.ui.date.setText("")
+            self.ui.time.setText("")
+            self.ui.i_o_n.setText("")
+
+        else:
+            msg_box = QMessageBox()
+            msg_box.setText("مشکلی پیش امده!!")
+            msg_box.exec_()
+
+    def remove_task(self,id):
+        feedback = self.db.remove_task(id)
+
+        if feedback == True:
+            self.read_from_database()
+
+        else:
+            msg_box = QMessageBox()
+            msg_box.setText("مشکلی پیش امده!!")
+            msg_box.exec_()
+    
+    def task_done(self,id):
+        feedback = self.db.task_done(id)
+
+        if feedback == True:
+            self.read_from_database()
 
         else:
             msg_box = QMessageBox()
@@ -34,19 +62,35 @@ class MainWindow(QMainWindow):
     def read_from_database(self):
         tasks = self.db.get_tasks()
 
-        is_dons = []
         for i in range(len(tasks)):
-            a = 1
             new_checkbox = QCheckBox()
-            new_label = QLabel()
-            new_btn = QPushButton("DEL")
-            new_label.setText(tasks[i][1]) 
+            new_label_n = QLabel()
+            new_label_d = QLabel()
+            new_label_date = QLabel()
+            new_label_time = QLabel()
+            new_btn_del = QPushButton("❌")
+            new_btn_del.setStyleSheet('background-color: rgb(255, 0, 0);')
             
-            self.ui.gl_tasks.addWidget(new_btn, i, 0)
-            self.ui.gl_tasks.addWidget(new_checkbox, i, 1)
-            self.ui.gl_tasks.addWidget(new_label, i, 2)
-            new_checkbox.clicked.connect(partial(self.db.task_done, tasks[i][0]))
-            new_btn.clicked.connect(partial(self.db.remove_task, tasks[i][0]))
+            new_label_n.setText(tasks[i][1])
+            new_label_d.setText(tasks[i][2]) 
+            new_label_date.setText(tasks[i][3])
+            new_label_time.setText(tasks[i][4])
+
+            if tasks[i][5] == 1: 
+                new_checkbox.setChecked(True)
+
+            if str(tasks[i][6]) == "important" or str(tasks[i][6]) == "Important":
+                new_label_n.setStyleSheet('color: rgb(255, 0, 0);')
+            
+            self.ui.gl_tasks.addWidget(new_checkbox, i, 0)
+            self.ui.gl_tasks.addWidget(new_label_n, i, 1)
+            self.ui.gl_tasks.addWidget(new_label_d, i, 2)
+            self.ui.gl_tasks.addWidget(new_label_date, i, 3)
+            self.ui.gl_tasks.addWidget(new_label_time, i, 4)
+            self.ui.gl_tasks.addWidget(new_btn_del, i, 5)
+
+            new_checkbox.clicked.connect(partial(self.task_done, tasks[i][0]))
+            new_btn_del.clicked.connect(partial(self.remove_task, tasks[i][0]))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
